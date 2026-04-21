0aa3f8de81cf7d981ef2225dc8ba744c6926fa48
jaswavj <jaswavj@gmail.com>
2026-04-21 17:33:50 +0530

    Initial commit

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v13.3.1 (64 bit)
-MySQL - 8.4.7 : Database - retail
+MySQL - 8.4.7 : Database - jangel1
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 8.4.7 : Database - retail
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`retail` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`jangel1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-USE `retail`;
+USE `jangel1`;
 
 /*Table structure for table `attender` */
 
